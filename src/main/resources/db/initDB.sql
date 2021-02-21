1560b419e57e72d66c24ea4aa50cdcf379c46a8c
MakSoap <cool.anton1@yandex.ru>
2021-02-21 19:10:11 +0300

    Completed task 2 from HW3

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 
 CREATE TABLE users
 (
@@ -11,6 +12,8 @@ registered       TIMESTAMP           DEFAULT now() NOT NULL,
 enabled          BOOL                DEFAULT TRUE  NOT NULL,
 calories_per_day INTEGER             DEFAULT 2000  NOT NULL
 );
+ALTER TABLE users AUTO_INCREMENT=100000;
+
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
 CREATE TABLE user_roles
@@ -21,4 +24,15 @@ CONSTRAINT user_roles_idx UNIQUE (user_id, role),
 FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-ALTER TABLE users AUTO_INCREMENT=100000;
+CREATE TABLE meals
+(
+id              INTEGER      AUTO_INCREMENT  PRIMARY KEY,
+user_id         INTEGER                         NOT NULL,
+datetime        DATETIME                        NOT NULL,
+description     VARCHAR(255)                    NOT NULL,
+calories        INTEGER                         NOT NULL,
+CONSTRAINT user_meals_unique_datetime_idx UNIQUE (user_id, datetime),
+FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+# CREATE UNIQUE INDEX meal_unique_datetime_idx ON meals (datetime);
+
--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,6 +1,9 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
+
 ALTER TABLE users AUTO_INCREMENT=100000;
+ALTER TABLE meals AUTO_INCREMENT=0;
 
 INSERT INTO users (name, email, password)
 VALUES ('User', 'user@yandex.ru', 'password'),
@@ -9,3 +12,14 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES (100000, '2020-01-30 10:00:00', 'Завтрак',                   500),
+       (100000, '2020-01-30 13:00:00', 'Обед',                      1000),
+       (100000, '2020-01-30 20:00:00', 'Ужин',                      500),
+       (100000, '2020-01-31 00:00:00', 'Еда на граничное значение', 100),
+       (100000, '2020-01-31 10:00:00', 'Завтрак',                   1000),
+       (100000, '2020-01-31 13:00:00', 'Обед',                      500),
+       (100000, '2020-01-31 20:00:00', 'Ужин',                      410),
+       (100001, '2015-06-01 14:00:00', 'Админ ланч',                510),
+       (100001, '2015-06-01 21:00:00', 'Админ ужин',                1500)
+       ;
