76dc16dbe695cc19285d966bc153b1df5fd6cb56
MakSoap <cool.anton1@yandex.ru>
2021-03-07 22:19:17 +0300

    Completed HW5 Optional p.5

--- src/test/java/ru/javawebinar/topjava/service/ServiceBaseTest.java
+++ src/test/java/ru/javawebinar/topjava/service/ServiceBaseTest.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.Stopwatch;
 import org.junit.runner.Description;
@@ -28,7 +29,12 @@ public abstract class ServiceBaseTest {
 
     private static final Logger log = getLogger("result");
 
-    private static final StringBuilder results = new StringBuilder();
+    private static StringBuilder results;
+
+    @BeforeClass
+    public static void initTestResult() {
+        results = new StringBuilder();
+    }
 
     @Rule
     // http://stackoverflow.com/questions/14892125/what-is-the-best-practice-to-determine-the-execution-time-of-the-bussiness-relev
